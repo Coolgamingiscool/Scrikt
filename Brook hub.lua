6542bca37266eadbbe4624c1dd47d3df3d6937a8
Coolgamingiscool <130761911+Coolgamingiscool@users.noreply.github.com>
2023-04-21 15:13:46 +0200

    Create Brook hub.lua

---
+++ Brook hub.lua
@@ -0,0 +1,279 @@
+local Library = loadstring(game:HttpGet("https://pastebin.com/raw/vff1bQ9F"))()
+
+local Window = Library.CreateLib("Brookhaven hack", "DarkTheme")
+
+--Plr tabs (done)
+
+local PlrTab = Window:NewTab("Clothes Hacks")
+
+local PlrSection = PlrTab:NewSection("Hacks")
+
+PlrSection:NewButton("Gold top", "Wears a gold top", function()
+
+    local args = {
+
+    [1] = "wear",
+
+    [2] = 6553199
+
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("JJ3"):WaitForChild("2NN2Updat2NN2eAvata2NN2r"):FireServer(unpack(args))
+
+end)
+
+PlrSection:NewButton("Gold pants", "Wears a gold pants", function()
+
+    local args = {
+
+    [1] = "wear",
+
+    [2] = 3548316019
+
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("JJ3"):WaitForChild("2NN2Updat2NN2eAvata2NN2r"):FireServer(unpack(args))
+
+end)
+
+PlrSection:NewButton("Black adidas", "Wears a gold adidas", function()
+
+    local args = {
+
+    [1] = "wear",
+
+    [2] = 6145141918
+
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("JJ3"):WaitForChild("2NN2Updat2NN2eAvata2NN2r"):FireServer(unpack(args))
+
+end)
+
+PlrSection:NewButton("Dominus white", "Wears a white dominus", function()
+
+    local args = {
+
+    [1] = "wear",
+
+    [2] = 21070012
+
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("JJ3"):WaitForChild("2NN2Updat2NN2eAvata2NN2r"):FireServer(unpack(args))
+
+end)
+
+PlrSection:NewButton("blue dominus", "Wears a blue dominus", function()
+
+    local args = {
+
+    [1] = "wear",
+
+    [2] = 48545806
+
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("JJ3"):WaitForChild("2NN2Updat2NN2eAvata2NN2r"):FireServer(unpack(args))
+
+end)
+
+--M tabs (done)
+
+local MTab = Window:NewTab("More Clothes Hacks")
+
+local MSection = MTab:NewSection("Hacks")
+
+MSection:NewButton("Black ghost", "Wears a black ghost", function()
+
+    local args = {
+
+    [1] = "wear",
+
+    [2] = 4932832113
+
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("JJ3"):WaitForChild("2NN2Updat2NN2eAvata2NN2r"):FireServer(unpack(args))
+
+end)
+
+MSection:NewButton("Angel wings", "Wears a angel wings", function()
+
+    local args = {
+
+    [1] = "wear",
+
+    [2] = 192557913
+
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("JJ3"):WaitForChild("2NN2Updat2NN2eAvata2NN2r"):FireServer(unpack(args))
+
+end)
+
+MSection:NewButton("Black mask", "Wears a black mask", function()
+
+    local args = {
+
+    [1] = "wear",
+
+    [2] = 928908332
+
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("JJ3"):WaitForChild("2NN2Updat2NN2eAvata2NN2r"):FireServer(unpack(args))
+
+end)
+
+MSection:NewButton("Jeff the killer mask", "Wears a jeff killer mask", function()
+
+    local args = {
+
+    [1] = "wear",
+
+    [2] = 5161514
+
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("JJ3"):WaitForChild("2NN2Updat2NN2eAvata2NN2r"):FireServer(unpack(args))
+
+end)
+
+MSection:NewButton("Black mask with red glasses", "Wears a black mask with red glasses", function()
+
+    local args = {
+
+    [1] = "wear",
+
+    [2] = 1778004652
+
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("JJ3"):WaitForChild("2NN2Updat2NN2eAvata2NN2r"):FireServer(unpack(args))
+
+end)
+
+--C tabs (done)
+
+local CTab = Window:NewTab("item hacks")
+
+local CSection = CTab:NewSection("Scripts")
+
+CSection:NewButton("money bag", "spawns moneybag", function()
+
+    local args = {
+
+    [1] = "2NN2Pick2NN2ingTo2NN2ols",
+
+    [2] = "DuffleBagMoney"
+
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("JJ3"):WaitForChild("2NN2T2NN2ool"):InvokeServer(unpack(args))
+
+end)
+
+CSection:NewButton("Ipad", "spawns a ipad", function()
+
+    local args = {
+
+    [1] = "2NN2Pick2NN2ingTo2NN2ols",
+
+    [2] = "Ipad"
+
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("JJ3"):WaitForChild("2NN2T2NN2ool"):InvokeServer(unpack(args))
+
+end)
+
+CSection:NewButton("iphone", "spawns a iphone ", function()
+
+    local args = {
+
+    [1] = "2NN2Pick2NN2ingTo2NN2ols",
+
+    [2] = "Iphone"
+
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("JJ3"):WaitForChild("2NN2T2NN2ool"):InvokeServer(unpack(args))
+
+end)
+
+--O tabs (done)
+
+local OTab = Window:NewTab("Other accessories")
+
+local OSection = OTab:NewSection("Scripts")
+
+OSection:NewButton("Banana", "ButtonInfo", function()
+
+    local args = {
+
+    [1] = "wear",
+
+    [2] = 4962552589
+
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("JJ3"):WaitForChild("2NN2Updat2NN2eAvata2NN2r"):FireServer(unpack(args))
+
+end)
+
+OSection:NewButton("box", "Buttoninfo", function()
+
+    local args = {
+
+    [1] = "wear",
+
+    [2] = 4753195371
+
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("JJ3"):WaitForChild("2NN2Updat2NN2eAvata2NN2r"):FireServer(unpack(args))
+
+end)
+
+OSection:NewButton("trash can", "Buttoninfo", function()
+
+    local args = {
+
+    [1] = "wear",
+
+    [2] = 4735272012
+
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("JJ3"):WaitForChild("2NN2Updat2NN2eAvata2NN2r"):FireServer(unpack(args))
+
+end)
+
+OSection:NewButton("tank", "Buttoninfo", function()
+
+    local args = {
+
+    [1] = "wear",
+
+    [2] = 4684079842
+
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("JJ3"):WaitForChild("2NN2Updat2NN2eAvata2NN2r"):FireServer(unpack(args))
+
+end)
+
+-- Others
+
+local OthersTab = Window:NewTab("Others")
+
+local OthersSection = OthersTab:NewSection("Scripts")
+
+OthersSection:NewButton("Infinite yield", "ButtonInfo", function()
+
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'))()
+
+end)
+
+--My first Epic Gui Hack
